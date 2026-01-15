b20f575467b7dc81d999bebf017cd4eb83b6d2f9
krishna <krishna613460@gmail.com>
2026-01-16 00:19:45 +0545

    SOcket added for real time chat in a community groups

---
+++ src/services/socket.js
@@ -0,0 +1,43 @@
+import { io } from 'socket.io-client';
+
+let socket = null;
+
+export const initializeSocket = (token) => {
+  if (socket && socket.connected) {
+    return socket;
+  }
+
+  socket = io('http://localhost:3000', {
+    auth: {
+      token
+    },
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionAttempts: 5
+  });
+
+  socket.on('connect', () => {
+    console.log('Socket connected:', socket.id);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected');
+  });
+
+  socket.on('connect_error', (error) => {
+    console.error('Socket connection error:', error);
+  });
+
+  return socket;
+};
+
+export const getSocket = () => {
+  return socket;
+};
+
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
